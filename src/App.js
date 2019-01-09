ab50691566c11eb8d05ee7e7f5b4957631653b18
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-09 23:27:03 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Rim', age: 29 },
+      { name: 'Sam', age: 30 },
+      { name: 'Sqkela', age: 28 },
+    ],
+    otherState: 'some other value'    
+  }
+
+  switchNameHandler = () => {
+    //console.log('click');
+    // DON'T DO THIS: this.state.persons[0].name = 'Rim4i4ok';
+    this.setState({
+      persons: [
+        { name: 'Rim4i4ok', age: 29 },
+        { name: 'Sam', age: 30 },
+        { name: 'Sqkela', age: 25 },
+      ]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>Simple text here</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Games</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>      
+    );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App'));
+  }
+}
+
+export default App;
