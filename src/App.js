b7cfe05941cd63b503b38dc14f7ff4b8194cea4f
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-10 22:32:39 +0300

    style

--- src/App.js
+++ src/App.js
@@ -37,12 +37,23 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>Simple text here</p>
         {/* bad way */}
-        <button onClick={() => this.switchNameHandler('Sloun')}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Sloun')}>Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
