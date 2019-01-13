ef28a8331052ec0809a2631430fcc4b826b3e267
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-13 13:55:47 +0300

    ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -61,12 +62,15 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person 
-              key={person.id}
-              click={() => this.deletePersonHandler(index)}
-              name={person.name} 
-              age={person.age} 
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
+            return  (
+              <ErrorBoundary key={person.id}>
+                <Person                   
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name} 
+                  age={person.age} 
+                  changed={(event) => this.nameChangedHandler(event, person.id)} />
+              </ErrorBoundary>
+            )
           })}          
         </div> 
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,8 +2,13 @@ import React from 'react';
 import classes from './Person.css';
 
 const person = ( props ) => {
+    const rnd = Math.random();
 
-    return (
+    if (rnd > 0.7) {
+        throw new Error('Something wrong');
+    }
+
+    return (  
         <div className={classes.Person}>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
