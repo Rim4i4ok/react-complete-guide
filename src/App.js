c23ea3fa8f15084e383d01e3128a2ba99abe7295
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-13 13:17:25 +0300

    npm eject + css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -84,18 +84,18 @@ class App extends Component {
     }
 
     //let classes = ['red', 'bold'].join(' ');
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red'); // classes = ['red]
+      assignedClasses.push(classes.red); // classes = ['red]
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold'); // classes = ['red', 'bold']
+      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
 
     return (      
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>Simple text here</p>
+          <p className={assignedClasses.join(' ')}>Simple text here</p>
           {/* bad way */}
           <button 
             style={style}
