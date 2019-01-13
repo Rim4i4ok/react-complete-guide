6fb25a5ea4a808fbb44ae14f131466c934e22173
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-13 23:19:04 +0300

    clear error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
