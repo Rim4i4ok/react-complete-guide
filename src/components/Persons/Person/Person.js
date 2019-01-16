621543e63838e4c8eef597e72a9b13166fd5b5b5
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-16 23:11:12 +0300

    16.3+ ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -10,6 +10,7 @@ class Person extends Component {
     constructor (props) {
         super(props);
         console.log('[Person.js] Inside constructor()', props);
+        this.inputElement = React.createRef();
     }
     
     componentWillMount() {
@@ -19,7 +20,7 @@ class Person extends Component {
     componentDidMount() {
         console.log('[Person.js] Inside componentDidMount()');
         if (this.props.position === 0) {
-            this.inputElement.focus();
+            this.inputElement.current.focus();
         }
     }
 
@@ -31,7 +32,7 @@ class Person extends Component {
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
-                    ref={(inp) => { this.inputElement = inp }}
+                    ref={this.inputElement}
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name} />
