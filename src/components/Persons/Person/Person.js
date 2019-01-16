d85b3df110952f43b59446a96d34597ebdf5db8c
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-16 23:01:17 +0300

    PropTypes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
@@ -23,7 +24,7 @@ class Person extends Component {
         console.log('[Person.js] Inside render()');
 
         return (  
-            <Aux classes={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
@@ -37,5 +38,12 @@ class Person extends Component {
         // ]
     }
 }
+
+Person.propTypes = {
+    click: PropTypes.func,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    changed: PropTypes.func
+};
 
 export default withClass(Person, classes.Person);
