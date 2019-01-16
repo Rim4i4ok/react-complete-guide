c44fe29f7b116d9c2c71f6aebb098de6b3dd4fa2
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-16 23:07:43 +0300

    ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -18,6 +18,9 @@ class Person extends Component {
 
     componentDidMount() {
         console.log('[Person.js] Inside componentDidMount()');
+        if (this.props.position === 0) {
+            this.inputElement.focus();
+        }
     }
 
     render () {
@@ -27,7 +30,11 @@ class Person extends Component {
             <Aux>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                    ref={(inp) => { this.inputElement = inp }}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name} />
             </Aux>
         );
         
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -42,6 +42,7 @@ class Persons extends PureComponent {
             return <Person 
                     key={person.id}                  
                     click={() => this.props.clicked(index)}
+                    position={index}
                     name={person.name} 
                     age={person.age} 
                     changed={(event) => this.props.changed(event, person.id)} />              
