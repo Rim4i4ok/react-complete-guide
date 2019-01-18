6b6303548100b6112f0ed1a286a5769c27781938
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-18 16:45:10 +0300

    new hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -45,6 +45,19 @@ class App extends PureComponent {
       console.log('[UPDATE App.js] Inside componentWillUpdate()', nextProps, nextState);
   }
 
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('[UPDATE App.js] Inside getDerivedStateFromProps()', nextProps, prevState);
+
+    return prevState;
+  }
+
+  getSnapshotBeforeUpdate() {
+    console.log('[UPDATE App.js] Inside getSnapshotBeforeUpdate()');
+
+    // save scroll pistion
+    return null;
+  }
+
   componentDidUpdate() {
       console.log('[UPDATE App.js] Inside componentDidUpdate()');
   }
